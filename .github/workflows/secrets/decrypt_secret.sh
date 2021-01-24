d529c2be9bbe4bb64877cb6d1fc67fb86c0e0209
Taylor Brown <taylor@thecloudtaylor.com>
2021-01-24 13:21:39 -0800

    Release Pipeline - Fix path

--- .github/workflows/secrets/decrypt_secret.sh
+++ .github/workflows/secrets/decrypt_secret.sh
@@ -5,4 +5,4 @@ mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$ENCRYPTION_KEY" \
---output $HOME/secrets/id_rsa package_repo.enc
+--output $HOME/secrets/id_rsa ./.github/workflows/secrets/package_repo.enc
